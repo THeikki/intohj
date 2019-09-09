8b868bdbd6f0af694cf88f2882d09e21324b4857
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-09-09 14:36:22 +0300

    html runko menulle valmis

---
+++ menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <div id="header">
+        This is the header
+    </div>
+    <div id="menu">
+        Menu
+    </div>
+    <div id="content">
+        Here is the content
+    </div>
+    <div id="footer">
+        This is the footer
+    </div>
+    
+</body>
+</html>
